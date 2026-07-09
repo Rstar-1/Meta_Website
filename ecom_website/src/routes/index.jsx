01c4b549361dd9f247c94c0c8de6616f9d4582b4
rajs <raj.shetye@sequretek.com>
2026-07-09 17:44:39 +0530

    mmm

--- ecom_website/src/routes/index.jsx
+++ ecom_website/src/routes/index.jsx
@@ -33,7 +33,6 @@ function AppRoutes() {
           <Route path="connect" element={<Contact />} />
           <Route path="about" element={<About />} />
           <Route path="cart" element={<Navigate to="/home" replace />} />
-          <Route path="supplier/:brandName" element={<Navigate to="/home" replace />} />
         </Route>
         <Route path="*" element={<h2 style={{ textAlign: 'center', padding: '100px 20px', fontFamily: 'Outfit, sans-serif' }}>404 - Page Not Found</h2>} />
       </Routes>
